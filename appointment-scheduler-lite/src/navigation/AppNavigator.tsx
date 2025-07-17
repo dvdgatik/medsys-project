77427ccd575b9a70867eb3e656e7f66f50e8d734
David Gatica <dvd_gatik@hotmail.com>
2025-07-17 10:44:46 -0600

    feat: add routing with react-router-dom

---
+++ appointment-scheduler-lite/src/navigation/AppNavigator.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import LoginScreen from '../screens/LoginScreen';
+import AppointmentListScreen from '../screens/AppointmentListScreen';
+import CreateAppointmentScreen from '../screens/CreateAppointmentScreen';
+import { useSelector, UseSelector } from 'react-redux';
+import { RootState } from '@reduxjs/toolkit/query';
+
+
+export type RootStackParamList = {
+    Login: undefined;
+    AppointmentList: undefined;
+    CreateAppointment: undefined;
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+const AppNavigator = () => {
+    const isLoggedIn = useSelector((state: any) => state.auth.isLoggedIn);
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                {isLoggedIn ? (
+                    <>
+                    <Stack.Screen name='AppointmentList' component={AppointmentListScreen} options={{'title': 'My Appointments'}}/>
+                    <Stack.Screen name='CreateAppointment' component={CreateAppointmentScreen} options={{'title': 'Create Appointment'}}/>
+                    </>
+                ): (
+                    <Stack.Screen name='Login' component={LoginScreen} options={{'title': 'Sign In'}}/>
+                )}
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+
+export default AppNavigator
+
+
+
+
---
+++ appointment-scheduler-lite/src/screens/CreateAppointmentScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+export default function CreateAppointmentScreen() {
+    return (
+        <View>
+            <Text>Create Appointment Screen</Text>
+        </View>
+    )
+}
